4aef6b323aed7e68b99a6d9cb7685e9ad3f7df11
Sohailtanveer1 <sohailtanveerforjobs@gmail.com>
2025-07-20 23:40:44 +0530

    silver script

--- Snowflake_ddl_scripts/SILVER_Scripts.sql
+++ Snowflake_ddl_scripts/SILVER_Scripts.sql
@@ -1,1 +1,72 @@
 -- This script create fact and dimension tables in the silver layer of Snowflake
+
+-- This script is intended to create the bronze layer tables in Snowflake
+CREATE TABLE ECOM.silver.customers (
+    customer_id INT PRIMARY KEY,
+    name VARCHAR(100),
+    city VARCHAR(50),
+    state VARCHAR(50),
+    country VARCHAR(50),
+    registration_date DATE,
+    is_active BOOLEAN,
+    etl_created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    etl_updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+);
+
+CREATE TABLE ECOM.silver.orders (
+    order_id INT PRIMARY KEY,
+    customer_id INT,
+    order_date DATE,
+    total_amount DECIMAL(10, 2),
+    status VARCHAR(20),
+    etl_created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    etl_updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    FOREIGN KEY (customer_id) REFERENCES customers(customer_id)
+);
+
+
+CREATE TABLE ECOM.silver.items (
+    item_id INT PRIMARY KEY,
+    item_name VARCHAR(100),
+    category VARCHAR(50),
+    price DECIMAL(10, 2),
+    etl_created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    etl_updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+);
+
+
+CREATE TABLE ECOM.silver.order_lines (
+    order_line_id SERIAL PRIMARY KEY,
+    order_id INT,
+    item_id INT,
+    quantity INT,
+    unit_price DECIMAL(10, 2),
+    etl_created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    etl_updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    FOREIGN KEY (order_id) REFERENCES orders(order_id),
+    FOREIGN KEY (item_id) REFERENCES items(item_id)
+);
+
+CREATE TABLE ECOM.silver.payments (
+    payment_id INT PRIMARY KEY,
+    order_id INT,
+    payment_date DATE,
+    amount DECIMAL(10, 2),
+    payment_method VARCHAR(50),
+    status VARCHAR(20),
+    etl_created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    etl_updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    FOREIGN KEY (order_id) REFERENCES orders(order_id)
+);
+
+CREATE TABLE ECOM..shippings (
+    shipping_id INT PRIMARY KEY,
+    order_id INT,
+    shipping_date DATE,
+    shipping_address VARCHAR(255),
+    shipping_method VARCHAR(50),
+    shipping_status VARCHAR(20),
+    etl_created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    etl_updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    FOREIGN KEY (order_id) REFERENCES orders(order_id)
+);
